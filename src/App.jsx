fdf347519cf26bace0ea191e2225d7b60647728f
gbabu0 <gbabu6203@gmail.com>
2025-04-03 14:35:03 +0530

    add content.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Header from './Header'
+import Content from './Content'
 
 function App() {
   
   return <>
   <Header />
+  <Content />
   </>
   
   
